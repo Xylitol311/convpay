b75381fce4d7d351293b5d1590f9cf8658cf985d
Xylitol311 <cktnghd0311@daum.net>
2023-10-13 02:10:54 +0900

    feat: Pay 기능 구현

--- src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
+++ src/main/java/com/zerobase/convpay/service/ConveniencePayService.java
@@ -1,17 +1,39 @@
 package com.zerobase.convpay.service;
 
-import com.zerobase.convpay.dto.PayRequest;
-import com.zerobase.convpay.dto.PayResponse;
-import com.zerobase.convpay.dto.PayResult;
+import com.zerobase.convpay.dto.*;
+import com.zerobase.convpay.type.MoneyUseCancelResult;
+import com.zerobase.convpay.type.MoneyUseResult;
+import com.zerobase.convpay.type.PayCancelResult;
+import com.zerobase.convpay.type.PayResult;
+
+import static com.zerobase.convpay.type.MoneyUseResult.USE_FAIL;
+import static com.zerobase.convpay.type.PayCancelResult.PAY_CANCEL_SUCCESS;
 
 public class ConveniencePayService {
+    private final MoneyAdapter moneyAdapter = new MoneyAdapter();
+    
     public PayResponse pay(PayRequest payRequest) {
+        MoneyUseResult moneyUseResult = moneyAdapter.use(payRequest.getPayAmount());
         
+        // Fail Case
+        if (moneyUseResult == USE_FAIL) {
+            return new PayResponse(PayResult.FAIL, 0);
+        }
+        
+        // Success Case
         return new PayResponse(PayResult.SUCCESS, payRequest.getPayAmount());
+        
     }
     
-    public void payCancel() {
-    
-    
+    public PayCancelReseponse payCancel(PayCancelRequest payCancelRequest) {
+        MoneyUseCancelResult moneyUseCancelResult = moneyAdapter.useCancel(payCancelRequest.getPayCancelAmount());
+        
+        if (moneyUseCancelResult == MoneyUseCancelResult.MONEY_USE_CANCEL_FAIL) {
+            return new PayCancelReseponse(PayCancelResult.PAY_CANCEL_FAIL, 0);
+        }
+        //success case
+        return new PayCancelReseponse(PAY_CANCEL_SUCCESS,
+                payCancelRequest.getPayCancelAmount());
+        
     }
 }
--- src/main/java/com/zerobase/convpay/service/MoneyAdapter.java
+++ src/main/java/com/zerobase/convpay/service/MoneyAdapter.java
@@ -1,11 +1,25 @@
 package com.zerobase.convpay.service;
 
+import com.zerobase.convpay.type.MoneyUseCancelResult;
+import com.zerobase.convpay.type.MoneyUseResult;
+
 public class MoneyAdapter {
-    public void use() {
-    
+    public MoneyUseResult use(Integer payAmount) {
+        System.out.println("MoneyAdapter.use : " + payAmount);
+        
+        if (payAmount > 1000000) { // 백만원 이상 너무 큰 금액은 실패로 처리
+            return MoneyUseResult.USE_FAIL;
+        }
+        return MoneyUseResult.USE_SUCCESS;
     }
     
-    public void useCancel() {
-    
+    public MoneyUseCancelResult useCancel(Integer payCancelAmount) {
+        System.out.println("MoneyAdapter.useCancel: " + payCancelAmount);
+        
+        if (payCancelAmount < 100) {
+            return MoneyUseCancelResult.MONEY_USE_CANCEL_FAIL;
+        }
+        
+        return MoneyUseCancelResult.MONEY_USE_CANCEL_SUCCESS;
     }
 }
